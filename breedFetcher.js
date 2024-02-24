077628b07e8a213cfda0c5ee6626803281e3baf2
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-24 15:17:15 -0800

    Refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
+// function definition
+const fetchBreedDescription = function(breedName , callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-const userInput = process.argv[2];
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
+  request(url, (error, respond, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (data.length < 1) {
+      callback('Not found', null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
 
-request(url, (error, respond, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  
-  const data = JSON.parse(body);
-
-  if (data.length < 1) {
-    console.log('Not found');
-    return;
-  }
-
-  console.log(data[0].description);
-});
+module.exports = {
+  fetchBreedDescription
+};
